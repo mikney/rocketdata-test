5140e25b799918f9ea3ff5fdb36a7fdcdc73a4be
Genadii <zherkovpasha@gmail.com>
2023-01-04 21:41:42 +0300

    added deploy and my cv link

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Test work for RocketData
-
+Deploy - https://thriving-chaja-e1e2e4.netlify.app/
 
 
 
--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,12 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Тестовая работа Zherka Pavel <a href="https://drive.google.com/file/d/1KYK4q-HbQbbxIzzMcGvzTdqhaTw_McKX/view" target='_blank' rel="noreferrer">CV</a></h1>
       <h1>1. HTML + CSS</h1>
       <BookPage />
       <h1>2. React</h1>
       <LoginForm />
-      <h1>3. Решить задачи из файлов - <a href='https://codesandbox.io/s/ts-part-forked-nrk5rv?file=/if.ts' target='_blank'>Codesandbox</a></h1>
+      <h1>3. Решить задачи из файлов - <a href='https://codesandbox.io/s/ts-part-forked-nrk5rv?file=/if.ts' target='_blank' rel="noreferrer">Codesandbox</a></h1>
 
     </div>
   );
